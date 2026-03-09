5f850aeb3f6e2b01aa52a22af20c71531629042d
Goodfeelings <34536150+Goodfeelings@users.noreply.github.com>
2026-03-09 01:01:31 -0400

    Added notebooks and PDFs for Exercises 3 and 4

--- utilities.py
+++ utilities.py
@@ -47,3 +47,37 @@ def check_data(dataframe, schema, print_rows=False):
         display(summary_df)
     else:
         print("No errors found.")
+
+comorbidity_schema = pa.DataFrameSchema({
+    # Comorbidity Columns are binary and should be 0 or 1.
+    # The ** allows us to unpack the dictionary into the schema.
+    # i.e., the same values make sense for all of the columns.
+    **{col: pa.Column(
+        int,
+        checks=pa.Check.isin([0, 1]),
+        nullable=False,
+        coerce=True
+    ) for col in [
+        'DIABETES', 'SMOKE', 'DYSPNEA', 'VENTILAT', 'HXCOPD', 'ASCITES',
+        'HXCHF', 'HYPERMED', 'RENAFAIL', 'DIALYSIS', 'DISCANCR', 'WNDINF',
+        'STEROID', 'WTLOSS', 'BLEEDIS', 'TRANSFUS'
+    ]},
+    # PRSEPIS should be one of the expected categorical values
+    "PRSEPIS": pa.Column(
+        str,
+        checks=pa.Check.isin(["None", "Sepsis", "Septic Shock", "SIRS"]),
+        nullable=False
+    ),
+    # BMI should be in a reasonable range
+    "BMI": pa.Column(
+        float,
+        checks=pa.Check.in_range(10, 150),
+        coerce=True
+    ),
+})
+
+derived_schema = pa.DataFrameSchema({
+    "SEPSIS": pa.Column(int, checks=pa.Check.isin([0, 1]), nullable=False),
+    "COMORBIDITIES": pa.Column(int, checks=pa.Check.isin([0, 1, 2, 3]),
+                               nullable=False),
+})
